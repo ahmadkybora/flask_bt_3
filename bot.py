a095b346e2b3bacd85cf7f89fedb37420f6c21fc
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-02-07 14:16:29 +0330

    =new

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 tkn = "2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo"
-updater = Updater(tkn,use_context=True)
+updater = Updater(tkn, use_context=True)
 bot = Bot(tkn)
 dispatcher : Dispatcher = updater.dispatcher
 
@@ -18,8 +18,12 @@ def start(update:Update, context:CallbackContext):
     chtiD = update.effective_message.chat_id
     username = update.effective_message.from_user.username
     txt = update.effective_message.text
+    # اینجا یک متغییر تعریف کردیم برای ساخت دکمه شیشه ای
+    # زمانی که کاربر بر روی هر کدام از دکمه های زیر کلیک میکن
+    # متن داخل بعنوان یک تکست برای ربات ارسال شده و شرط مورد نظر 
+    # انجام میشود
     keyboard = [
-        [KeyboardButton('Help')],
+        [KeyboardButton('Start')],
         [KeyboardButton('Contact us')]
     ]
     key = ReplyKeyboardMarkup(keyboard,resize_keyboard=True)
@@ -31,7 +35,7 @@ def start(update:Update, context:CallbackContext):
             text="How to Deploy Your Telegram bot on Heroku\n\nچگونه ربات خود را در Heroku راه اندازی کنید",
             reply_to_message_id=update.effective_message.message_id,
         )
-    elif txt=="Contact us":
+    elif txt=="Start":
         bot.send_message(
             chat_id=chtiD,
             text="<u>سلام</u>\n\n<i>Telegram : </i>خوبی",
@@ -50,7 +54,12 @@ def start(update:Update, context:CallbackContext):
 
 def main():
 
+    # برای مدیریت پیام های غیر دستوری از کلاس
+    # MessageHandler
+    # استفاده میکنیم منظور پیام هایی که بتعرفشان نکردیم
     dispatcher.add_handler(MessageHandler(Filters.text,start))
+
+    # متد زیر برای ران کردن بات است
     updater.start_polling()
 
 
